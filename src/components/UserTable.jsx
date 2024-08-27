4f08891adbfe7496e8369810e707a391f8077509
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-08-27 10:43:48 +0530

    final

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -30,28 +30,44 @@ const UserTable = () => {
 
             {users.length === 0 && !loading && !error && <p>No users found.</p>}
 
-            {users.length > 0 && (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Avatar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(user => (
-                            <tr key={user.id}>
-                                <td>{user.first_name}</td>
-                                <td>{user.last_name}</td>
-                                <td>{user.email}</td>
-                                <td><img src={user.avatar} alt={`${user.first_name}'s avatar`} width="50" /></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
+           {users.length > 0 ? (
+    <table>
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Avatar</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users.map(user => (
+                <tr key={user.id}>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.email}</td>
+                    <td><img src={user.avatar} alt={`${user.first_name}'s avatar`} width="50" /></td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+) : (
+    <table>
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Avatar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colSpan="4">No data found</td>
+            </tr>
+        </tbody>
+    </table>
+)}
         </div>
     );
 };
